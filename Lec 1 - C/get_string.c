30d49dd0b45f59f9186d1732f879d9f992481357
nhsamng <nhungntk.yec20@gmail.com>
2025-05-09 02:09:41 +0700

    delete fflush

--- Lec 1 - C/get_string.c
+++ Lec 1 - C/get_string.c
@@ -2,7 +2,6 @@
 #include <stdio.h>
 int main(void)
 {
-    /* code */
     string answer = get_string("What's your name? \n");
-    printf ("hello, %s", answer); 
+    printf ("hello, %s", answer);
 }
