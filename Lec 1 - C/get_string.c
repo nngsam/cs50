dd906d5a37fd7addf27ce05bcfa154c1de69fc95
nngsam <11203041@st.neu.edu.vn>
2025-06-03 05:58:52 +0700

    feat: add comments

--- Lec 1 - C/get_string.c
+++ Lec 1 - C/get_string.c
@@ -1,5 +1,6 @@
 #include <cs50.h>
-#include <stdio.h>
+#include <stdio.h> /* standard input output dot h, is ta famous file in c TO get input & output from user */
+
 int main(void)
 {
     string answer = get_string("What's your name? \n");
--- Lec 1 - C/hello.c
+++ Lec 1 - C/hello.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
 int main() {
+    // Greet Human
     printf("Hello, World!\n");
-    return 0;
 }
