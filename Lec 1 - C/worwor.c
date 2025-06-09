68fb4606968fa1787e3166b5003016396f1ef99b
nngsam <11203041@st.neu.edu.vn>
2025-06-09 16:35:01 +0700

    feat: put counter to value-return function

--- Lec 1 - C/worwor.c
+++ Lec 1 - C/worwor.c
@@ -1,23 +1,25 @@
 #include <stdio.h>
 
 
-// there will exist the function called worwor, but just not yet
-void worwor(void);
+// Declare the function and say it returns an int
+int worwor(int n);
+
 
 int main (void)
 {   
-    int count = 0;
-    for (int i = -1; i <=4; i+=1)
-    {
-        worwor();
-        count += 1;
-    }
-
+    int count = worwor(3);
     printf("your dog bark %i times\n", count);
 }
 
-// i told u, it exists
-void worwor(void)
-{
-    printf("wor wor\n");
+
+// Define worwor to return the number of barks
+int worwor(int n)
+{   
+    int bark_count = 0;
+    for (int i = -1; i <=n; i+=1)
+    {
+        printf ("worwor\n");
+        bark_count += 1;
+    }
+    return bark_count;
 }
