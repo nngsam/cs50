ee9f2c211e3afc163c090dbca21aebf5f61ce1b9
nngsam <11203041@st.neu.edu.vn>
2025-06-14 12:17:51 +0700

    debug by printing out

--- Lec 2 - Array/buggy0.c
+++ Lec 2 - Array/buggy0.c
@@ -5,7 +5,7 @@ int main (void)
     // print 10 hashes 
     for (int i=0; i<=9; i++)
     {
-        printf("i is now %i\n", i);
+        // printf("i is now %i\n", i);
         printf("#\n");
     }
 }
