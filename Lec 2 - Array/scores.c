06b4184273044d1cd153837d97c3863743468b55
nngsam <11203041@st.neu.edu.vn>
2025-06-16 18:44:54 +0700

    feat: add functional avg calculation

--- Lec 2 - Array/scores.c
+++ Lec 2 - Array/scores.c
@@ -2,18 +2,29 @@
 #include <cs50.h>
 
 
+float average(int length, int array[]); 
+
 int main(void)
 {
-    int num;
-    num = get_int("how many num: \n");
+    int num = get_int("how many num: \n");
 
     int scores[num];
     float sum;
     for (int i=0; i < num; i++)
     {
         scores[i] = get_int("score[%i]: \n", i);
-        sum += scores[i];
     }
-    printf("sum = %.1f\n", sum);
-    printf("average: %.1f\n", sum/num);
+    printf("average: %.1f\n", average(num,scores));
+}
+
+
+float average(int length, int array[])
+{
+    float sum = 0; 
+    for (int i=0; i < length; i++)
+    {
+        sum += array[i];
+    }
+
+    return sum/length;
 }
