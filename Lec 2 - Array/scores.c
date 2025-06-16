1ab8602615e30de98abe1dbda4f083ce85afb7bd
nngsam <11203041@st.neu.edu.vn>
2025-06-16 19:02:06 +0700

    feat: fixed size array

--- Lec 2 - Array/scores.c
+++ Lec 2 - Array/scores.c
@@ -2,18 +2,19 @@
 #include <cs50.h>
 
 
+const int TOTAL=2;
 float average(int length, int array[]); 
 
 int main(void)
 {
-    int num = get_int("how many num: \n");
+    //int num = get_int("how many num: \n");
 
-    int scores[num];
-    for (int i=0; i < num; i++)
+    int scores[TOTAL];
+    for (int i=0; i < TOTAL; i++)
     {
         scores[i] = get_int("score[%i]: \n", i);
     }
-    printf("average: %.1f\n", average(num, scores));
+    printf("average: %.1f\n", average(TOTAL, scores));
 }
 
 
