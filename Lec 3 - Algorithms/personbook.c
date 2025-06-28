f0275c21d6431a451a96c2afab7c58122080a2bf
nngsam <11203041@st.neu.edu.vn>
2025-06-28 20:15:42 +0700

    chore: add typedef of a person

--- Lec 3 - Algorithms/personbook.c
+++ Lec 3 - Algorithms/personbook.c
@@ -3,16 +3,33 @@
 #include <string.h>
 
 
+typedef struct
+{
+    string name;
+    string number;
+}person;
+
+const int NUMBER = 2;
+
 int main(void)
 {
-    string names[] = {"bill", "charles", "fred", "george", "ginny", "percy", "ron"};
-    string numbers[] = {"4", "6", "8", "2", "7", "5", "0"};
+    // string names[] = {"bill", "charles", "fred", "george", "ginny", "percy", "ron"};
+    // string numbers[] = {"4", "6", "8", "2", "7", "5", "0"};
 
-    for (int i=0; i<7; i++)
+    person people[NUMBER];
+
+    people[0].name = "bill"; 
+    people[1].name = "charles"; 
+
+    people[0].number = "4"; 
+    people[1].number = "6"; 
+
+
+    for (int i=0; i<NUMBER; i++)
     {
-        if (strcmp(names[i], "ron") == 0) 
+        if (strcmp(people[i].name, "bill") == 0) 
         {
-            printf("found that person with phonenumber %s\n", numbers[i]);
+            printf("found that person with phonenumber %s\n", people[i].number);
             return 0;
         }
     }
