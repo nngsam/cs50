37b77eb476764c5307bbb2b12554f6304ae7af23
nngsam <11203041@st.neu.edu.vn>
2025-07-02 18:30:38 +0700

    feat: add comments

--- Lec 3 - Algorithms/linear_search.c
+++ Lec 3 - Algorithms/linear_search.c
@@ -1,22 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <cs50.h>
 
 
 int main(int argc, char *argv[])
 {
     if (argc < 3) // ít nhất: ./search target num1
     {
-        printf("Usage: ./search <target> <num1> <num2> ...\n");
-        return 1;
+        fprintf(stderr, "error: not enough arguments\n");
+        fprintf(stderr, "usage: ./linear_search <target> <num1> <num2> ...\n");
+        // return 1;
+        exit(EXIT_FAILURE);  // giống raise Exception
     }
 
     int target = atoi(argv[1]);
-    int array_len = argc - 2;
+    int array_len = argc - 2;  // độ dài của mảng 
 
     int numbers[array_len];
-    for (int i = 0; i < array_len; i++)
+    for (int i = 0; i < array_len; i++) 
     {
-        numbers[i] = atoi(argv[i + 2]);
+        numbers[i] = atoi(argv[i + 2]);  // chuyển đổi phần còn lại thành số và lưu vào mảng 
     }
 
     // Linear search
